2d6830e80dfa158cd4b2634888d24d3599aced23
Mirsaid <akbaraliyev522@mail.ru>
2024-09-08 00:22:56 +0500

    first commit

---
+++ src/data.js
@@ -0,0 +1,86 @@
+
+export const hero = [
+    {
+        heroLamp: "../images/image 32.svg",
+        heroDivan: "../images/Shell-Shaped-Armchair-Pink-Velvet-Fabric-One-Seater-Sofa-for-Living-Room 1.svg",
+        titleHead: "Best Furniture For Your Castle....",
+        titleMain: "New Furniture Collection Trends in 2020",
+        titlelist: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Magna in est adipiscing in phasellus non in justo.",
+        shopnow: "Shop Now"
+    }
+]
+
+export const product = [
+    {
+        titleProduct: "Featured Products",
+        cards:[
+            {
+                cardImage: "../images/card1.svg",
+                cardTitle: "Cantilever chair",
+                cardList: "Code - Y523201",
+                cardPrice: "$42.00"
+            },
+            {
+                cardImage: "../images/card2.svg",
+                cardTitle: "Cantilever chair",
+                cardList: "Code - Y523201",
+                cardPrice: "$42.00"
+            },
+            {
+                cardImage: "../images/card3.svg",
+                cardTitle: "Cantilever chair",
+                cardList: "Code - Y523201",
+                cardPrice: "$42.00"
+            },
+            {
+                cardImage: "../images/card4.svg",
+                cardTitle: "Cantilever chair",
+                cardList: "Code - Y523201",
+                cardPrice: "$42.00"
+            }
+        ]
+    }
+]
+
+
+export const leatestProduct = [
+    {
+        testproduct: "Leatest Products",
+        newArrival: [
+            {
+                testImage:"../images/test1.svg",
+                info: "Comfort Handy Craft",
+                Price: "$42.00 $65.00"
+            },
+            {
+                testImage: "../images/test2.svg",
+                info: "Comfort Handy Craft",
+                Price: "$42.00 $65.00"
+            },
+            {
+                testImage: "../images/test3.svg",
+                info: "Comfort Handy Craft",
+                Price: "$42.00 $65.00"
+            },
+            {
+                testImage: "../images/test4.svg",
+                info: "Comfort Handy Craft",
+                Price: "$42.00 $65.00"
+            },
+            {
+                testImage: "../images/test5.svg",
+                info: "Comfort Handy Craft",
+                Price: "$42.00 $65.00"
+            },
+            {
+                testImage: "../images/test6.svg",
+                info: "Comfort Handy Craft",
+                Price: "$42.00 $65.00"
+            }
+        ],
+        bestSeller: [],
+        Featured: [],
+        specialOffer: []
+
+    }
+]
---
+++ src/main.js
@@ -0,0 +1,2 @@
+import { hero,product,leatestProduct, } from "./data.js";
+
